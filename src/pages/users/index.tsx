489f85c6424a0876f582a96b79b520ed454ea0eb
Leonardo <leonardo.pacciulli@opah.com.br>
2021-11-10 18:27:31 -0300

    Fetching users with react-query

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -12,7 +12,20 @@ export default function UsersList() {
     const response = await fetch('http://localhost:3000/api/users')
     const data = await response.json();
 
-    return data
+    const users = data.users.map((user) => {
+      return {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric'
+        })
+      }
+    })
+
+    return users
   })
 
   const isWideVersion = useBreakpointValue({
@@ -74,20 +87,24 @@ export default function UsersList() {
                     </Tr>
                   </Thead>
                   <Tbody>
-                    <Tr>
-                      <Td px={["4", "4", "6"]}>
-                        <Checkbox colorScheme="pink" />
-                      </Td>
-                      <Td>
-                        <Box>
-                          <Text fontWeight="bold">Leonardo</Text>
-                          <Text color="gray.300" fontSize="small">leo@gmail.com</Text>
-                        </Box>
-                      </Td>
-                      {isWideVersion && (
-                        <Td>04 de Abril, 2021</Td>
-                      )}
-                    </Tr>
+                      {data.map((user) => {
+                        return (
+                          <Tr key={user.id}>
+                            <Td px={["4", "4", "6"]}>
+                              <Checkbox colorScheme="pink" />
+                            </Td>
+                            <Td>
+                              <Box>
+                                <Text fontWeight="bold">{user.name}</Text>
+                                <Text color="gray.300" fontSize="small">{user.email}</Text>
+                              </Box>
+                            </Td>
+                            {isWideVersion && (
+                              <Td>{user.createdAt}</Td>
+                            )}
+                          </Tr>
+                        )
+                      })}
                   </Tbody>
                 </Table>
 
