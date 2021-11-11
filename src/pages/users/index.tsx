8ac7765d8d89686cad823dd1e38d08e95d7ada9b
Leonardo <leonardo.pacciulli@opah.com.br>
2021-11-11 18:19:33 -0300

    Creating hook useUsers

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -4,39 +4,14 @@ import Sidebar from '../../components/Sidebar'
 import { RiAddLine } from 'react-icons/ri'
 import { Pagination } from '../../components/Pagination'
 import Link from 'next/link'
-import { useEffect } from 'react'
-import { useQuery } from 'react-query'
+import { useUsers } from '../../services/hooks/useUsers'
 
 export default function UsersList() {
-  const { data, isLoading, error } = useQuery('users', async () => {
-    const response = await fetch('http://localhost:3000/api/users')
-    const data = await response.json();
-
-    const users = data.users.map((user) => {
-      return {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
-          day: '2-digit',
-          month: 'long',
-          year: 'numeric'
-        })
-      }
-    })
-
-    return users
-  }, {
-    staleTime: 1000 * 5
-  })
+  const { data, isLoading, isFetching, error } = useUsers()
 
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true
-  })
-
-  useEffect(() => {
-    
   })
 
   return (
@@ -54,7 +29,17 @@ export default function UsersList() {
 
         <Box flex="1" borderRadius={8} bg="gray.800" p="8">
           <Flex mb="8" justify="space-between" align="center">
-            <Heading size="lg" fontWeight="normal">Usuários</Heading>
+            <Heading size="lg" fontWeight="normal">
+              Usuários
+
+              {!isLoading && isFetching && (
+                <Spinner 
+                  size="sm"
+                  color="gray.500"
+                  ml="4"
+                />
+              )}
+            </Heading>
             <Link href="/users/create" passHref>
               <Button as="a" size="sm" fontSize="sm" colorScheme="pink" leftIcon={<Icon as={RiAddLine} fontSize="20" />}>
                 Criar novo
